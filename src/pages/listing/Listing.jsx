a1288d226de51f4b1a89f4fbe8168094eb5760b7
Fabio Lanza <fabio.lanza4@gmail.com>
2023-04-08 07:23:58 -0700

    added listing info

--- src/pages/listing/Listing.jsx
+++ src/pages/listing/Listing.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../../firebase";
-import { FaShare } from "react-icons/fa";
+import { FaShare, FaBed, FaBath, FaParking, FaChair } from "react-icons/fa";
+import { MdLocationOn } from "react-icons/md";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, {
   EffectFade,
@@ -12,7 +13,7 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
-import "./Listing.css";
+
 
 export default function Listing() {
   const params = useParams();
@@ -54,6 +55,8 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+
+      {/* Copy button */}
       <div
         className="fixed top-[13%] right-[3%] z-10
        bg-white cursor-pointer border-2 border-gray-500
@@ -68,9 +71,71 @@ export default function Listing() {
       >
         <FaShare className="text-lg text-slate-600" />
       </div>
-      {shareLinkCopied && <p className="fixed top-[20%] right-[5%]
-         font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link copied</p>  
-      }
+      {shareLinkCopied && (
+        <p
+          className="fixed top-[20%] right-[5%]
+         font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2"
+        >
+          Link copied
+        </p>
+      )}
+
+      {/* //Listing Details */}
+      <div
+        className="m-4 flex flex-col md:flex-row max-w-6xl
+       lg:mx-auto p-4 rounded-lg bg-white lg:space-x-5"
+      >
+        <div className="w-full h-[200px] lg-[400px]">
+          <p className="text-[22px] font-bold mb-7 text-blue-900">
+            {listing?.name} - ${" "}
+             {listing?.offer
+              ? listing?.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing?.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing?.type === "rent" ? " / month" : ""}
+          </p>
+          <div className="flex font-semibold mt-6 mb-3">
+            <MdLocationOn
+              style={{ color: "green", fontSize: "20px", marginRight: "5px" }}
+            />
+            <p>{listing?.address}</p>
+          </div>
+          <div>
+            <p className="bg-red-500 max-w-[150px] 
+            p-1 text-center text-[16px] text-white rounded-md">
+              {listing?.type === "rent" ? "Rent" : "Sale"}
+              </p>
+           
+          </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold">Description: </span>
+            {listing?.description}</p>
+
+            <ul className="flex space-x-3 lg:space-x-10 font-semibold">
+              <li className="flex items-center whitespace-nowrap">
+                <FaBed className="tx-lg mr-1"/>
+                {+listing?.bedroom > 1 ? `{listing.bedroom}Beds` : "1 Bed"}
+              </li>
+              <li className="flex items-center whitespace-nowrap">
+                <FaBath className="tx-lg mr-1"/>
+                {+listing?.bathroom > 1 ? `{listing.bathroom}Baths` : "1 Bath"}
+              </li>
+              <li className="flex items-center whitespace-nowrap">
+                <FaParking className="tx-lg mr-1"/>
+                {+listing?.parking ? "Parking Spot" : "No parking"}
+              </li>
+              <li className="flex items-center whitespace-nowrap">
+                <FaChair className="tx-lg mr-1"/>
+                {+listing?.furnished ? "Furnished" : "No furnished"}
+              </li>
+            </ul>
+        </div>
+
+        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+      </div>
     </main>
   );
 }
